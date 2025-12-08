78273acb6f16b0542c9d288c0cf93235f4ac42de
Mohamed Elfeshawy <mh3386240@gmail.com>
2025-12-08 17:48:00 +0200

    feat: create Transaction entity

---
+++ src/main/java/com/jpmc/midascore/entity/Transaction.java
@@ -0,0 +1,76 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    @Column(nullable = false)
+    private float amount;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private UserRecord sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private UserRecord recipient;
+
+    public Transaction() {
+    }
+
+    public Transaction(float amount, UserRecord sender, UserRecord recipient) {
+        this.amount = amount;
+        this.sender = sender;
+        this.recipient = recipient;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public UserRecord getSender() {
+        return sender;
+    }
+
+    public void setSender(UserRecord sender) {
+        this.sender = sender;
+    }
+
+    public UserRecord getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(UserRecord recipient) {
+        this.recipient = recipient;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", timestamp=" + timestamp +
+                ", sender=" + sender +
+                ", recipient=" + recipient +
+                '}';
+    }
+}
