5a1f94d7ed07d49df3f6c1f437e27d9218424371
Mohamed Elfeshawy <mh3386240@gmail.com>
2025-12-09 16:14:00 +0200

    feat: implement Kafka listener for transaction consumption

---
+++ src/main/java/com/jpmc/midascore/listener/TransactionListener.java
@@ -0,0 +1,20 @@
+package com.jpmc.midascore.listener;
+
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.service.transaction.TransactionService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+    private final TransactionService transactionService;
+
+    public TransactionListener(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void listen(Transaction dto) {
+        transactionService.processTransaction(dto);
+    }
+}
